31d72601788e64b66b9ae2257ea1322b40a60a67
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-10-05 16:07:01 +0600

    sorver created

--- .eslintrc-config.js
+++ .eslintrc-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "no-undef": "error",
-    "no-unused-expressions": "error",
-    "no-unreachable": "error",
-    "prettier/prettier": "error",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'no-undef': 'error',
+    'no-unused-expressions': 'error',
+    'no-unreachable': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   env: {
     browser: true,
@@ -20,6 +20,6 @@ module.exports = {
     node: true,
   },
   globals: {
-    process: "readonly",
+    process: 'readonly',
   },
 };
